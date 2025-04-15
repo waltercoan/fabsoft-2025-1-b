e861bf2c9a9e4e735c9419440cffa785d408b98a
WALTER SILVESTRE COAN <walter.s@univille.br>
2025-04-14 22:16:34 -0300

    services

---
+++ projfabsoft/src/main/java/br/univille/projfabsoft/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,45 @@
+package br.univille.projfabsoft.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.univille.projfabsoft.entity.Cliente;
+import br.univille.projfabsoft.repository.ClienteRepository;
+import br.univille.projfabsoft.service.ClienteService;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+    
+    @Autowired
+    private ClienteRepository repository;
+
+    @Override
+    public Cliente save(Cliente cliente) {
+        repository.save(cliente);
+        return cliente;
+    }
+
+    @Override
+    public List<Cliente> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Cliente getById(Long id) {
+        var retorno = repository.findById(id);
+        if(retorno.isPresent())
+            return retorno.get();
+        return null;
+    }
+
+    @Override
+    public Cliente delete(Long id) {
+        var cliente = getById(id);
+        if(cliente != null)
+            repository.deleteById(id);
+        return cliente;
+    }
+
+}
