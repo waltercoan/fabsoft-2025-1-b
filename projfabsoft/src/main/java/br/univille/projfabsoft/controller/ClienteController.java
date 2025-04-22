cfedc51954aa0d99f8ad45eabcf7bef4e3dd358d
WALTER SILVESTRE COAN <walter.s@univille.br>
2025-04-22 20:22:54 -0300

    put and delete

--- projfabsoft/src/main/java/br/univille/projfabsoft/controller/ClienteController.java
+++ projfabsoft/src/main/java/br/univille/projfabsoft/controller/ClienteController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,4 +42,46 @@ public class ClienteController {
         }
         return ResponseEntity.badRequest().build();
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> 
+        putCliente(@PathVariable long id,
+            @RequestBody Cliente cliente){
+        
+        if(id <= 0 || cliente == null){
+            return ResponseEntity.badRequest().build();
+        }
+        var clienteAntigo = service.getById(id);
+        if(clienteAntigo == null){
+            return ResponseEntity.notFound().build();
+        }
+        clienteAntigo.setNome(cliente.getNome());
+        clienteAntigo.setEndereco(cliente.getEndereco());
+        clienteAntigo.setTelefone(cliente.getTelefone());
+        clienteAntigo.setEmail(cliente.getEmail());
+        clienteAntigo.setDataNascimento(cliente.getDataNascimento());
+
+        service.save(clienteAntigo);
+        return new ResponseEntity<Cliente>(clienteAntigo,
+                HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cliente> deleteCliente(@PathVariable long id){
+        if(id <=0){
+            return ResponseEntity.badRequest().build();
+        }
+
+        var clienteExcluido = service.getById(id);
+        if(clienteExcluido == null){
+            return ResponseEntity.notFound().build();
+        }
+        service.delete(id);
+
+        return new ResponseEntity<Cliente>(clienteExcluido,
+                HttpStatus.OK);
+    }
+
+
+
+
 }
